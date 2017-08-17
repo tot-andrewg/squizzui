01f0ea9f01e5b6b4e26f888cba7e7b886a168909
tot-andrewg <andrewg@totecs.com>
2017-08-17 16:19:51 +1000

    Modified contacts within conversation Added conversation attachments

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+
+//$(document).ready(function(e, delta) {
+//    $('.conversation-contact-listing').scroll(function(e, delta) {
+//        $(this).scrollLeft($(this).scrollLeft = (delta * 10));
+//        e.preventDefault();
+//    })
+//});
